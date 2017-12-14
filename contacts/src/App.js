3764e4ec36a73ad34b1dd842acf13d094e6756b5
Ankit Komawar <komawarankit67@gmail.com>
2017-12-14 02:38:10 -0800

    Finishing the contact app

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -21,6 +21,14 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then((contact) => {
+      this.setState(state => ({
+        contacts: this.state.contacts.concat( [ contact ] )
+      })
+      )
+    })
+  }
   render(){
     return(
       <div>
@@ -31,7 +39,15 @@ class App extends Component {
           />
         )}
         />
-        <Route path='/create' component={CreateContact}/>
+        <Route path='/create' render= {({history})=>(
+          <CreateContact
+            onCreateContact={(contact)=>{
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+          )}
+        />
       </div>
     )
   }
