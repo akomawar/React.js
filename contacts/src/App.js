50fb9ec6ce35f4dc18902d6244dec8ab58ebb1c7
Ankit Komawar <komawarankit67@gmail.com>
2017-12-13 23:25:50 -0800

    Added button for create contact

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,39 +1,45 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
-
+import CreateContact from './CreateContact'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    screen: 'list', //list or create
+    contacts: []
   }
-removeContact = (contact) => {
+  componentDidMount(){
+    ContactsAPI.getAll().then((contacts)=>{
+      this.setState({ contacts })
+    })
+  }
+
+  removeContact = (contact) => {
     this.setState((state) => ({
       contacts: this.state.contacts.filter((c) => c.id !== contact.id)
     }))
-}
+    ContactsAPI.remove(contact)
+  }
+
+  navigateToCreate = (contact) =>{
+    this.setState({screen:'create'})
+  }
 
   render(){
     return(
-      <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
+      <div>
+        {this.state.screen === "list" && (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+            onNavigate={this.navigateToCreate}
+          />
+
+        )}
+        {this.state.screen === "create" &&(
+          <CreateContact/>
+        )}
+      </div>
     )
   }
 }
---
+++ contacts/src/CreateContact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CreateContact extends Component{
+  render(){
+    return(
+      <p>Hello New Contact!</p>
+    )
+  }
+}
+
+export default CreateContact
