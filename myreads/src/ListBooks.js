b2076745d8a366f37025d93e02c38fcb48e6907a
Ankit Komawar <komawarankit67@gmail.com>
2017-12-16 19:00:47 -0800

    Heading created

---
+++ myreads/src/ListBooks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class ListBooks extends Component{
+  render(){
+    return(
+      <div className='top-bar'>
+        <h1 className="top-bar-heading">MyReads</h1>        
+      </div>
+    )
+  }
+}
+
+
+export default ListBooks
