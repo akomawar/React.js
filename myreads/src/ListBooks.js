8ec4ed1dcbf2bdaff5aab17fc77c6018ed3fbdd1
Ankit Komawar <komawarankit67@gmail.com>
2017-12-20 20:47:03 -0800

    cleaning code

--- myreads/src/ListBooks.js
+++ myreads/src/ListBooks.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 class ListBooks extends Component{
   render(){
     return(
-      <div className='top-bar'>
-        <h1 className="top-bar-heading">MyReads</h1>        
+      <div className='list-books'>
+
+        <div className='list-books-title'>
+          <h1 className="top-bar-heading">MyReads</h1>
+        </div>
+
+        <div className="list-books-content">
+          <h2>Currently Reading</h2>
+          <h2>Want to Read</h2>
+          <h2>Read</h2>
+        </div>
+
       </div>
     )
   }
