ba1a3c97cc568467f4cfb3eb77f25e4606becd58
Ankit Komawar <komawarankit67@gmail.com>
2017-12-20 22:14:00 -0800

    first picture of book

--- myreads/src/ListBooks.js
+++ myreads/src/ListBooks.js
@@ -10,19 +10,41 @@ class ListBooks extends Component{
         </div>
 
         <div className="list-books-content">
+          <div>
 
+{/*Currently Reading Section*/}
             <div className='bookshelf'>
               <h2>Currently Reading</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    <li>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                        </div>
+                      </div>
+                    </li>
+                  </ol>
+                </div>
             </div>
-            
+
+{/*Want to Read Section*/}
             <div className='bookshelf'>
               <h2>Want to Read</h2>
+                <div className="bookshelf-books">
+                  2
+                </div>
             </div>
 
+{/*Read Section*/}
             <div className='bookshelf'>
               <h2>Read</h2>
+                <div className="bookshelf-books">
+                  3
+                </div>
             </div>
 
+          </div>
         </div>
 
       </div>
